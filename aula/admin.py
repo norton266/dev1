a50a034b2dbbc8a6aab0aef75490f462786e67f4
root <root@DESKTOP-MDN68C5.>
2025-03-24 22:04:06 -0300

    aula2403

--- aula/admin.py
+++ aula/admin.py
@@ -3,6 +3,7 @@ from aula.models import Person
 from aula.models import Passport
 from aula.models import Reporter
 from aula.models import Article
+from aula.models import Magazine
 
 
 # Register your models here.
@@ -11,3 +12,4 @@ admin.site.register(Person)
 admin.site.register(Passport)
 admin.site.register(Reporter)
 admin.site.register(Article)
+admin.site.register(Magazine)
--- aula/models/__init__.py
+++ aula/models/__init__.py
@@ -3,4 +3,5 @@ from .passport import *
 from .person import *
 from .article import *
 from .reporter import *
+from .magazine import *
 
