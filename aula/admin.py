bcf72ada0d6c45165505bdaaecb505b1089c3159
root <root@DESKTOP-MDN68C5.>
2025-03-24 21:54:45 -0300

    aula2403

--- aula/admin.py
+++ aula/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 from aula.models import Person
 from aula.models import Passport
+from aula.models import Reporter
+from aula.models import Article
+
 
 # Register your models here.
 
 admin.site.register(Person)
 admin.site.register(Passport)
+admin.site.register(Reporter)
+admin.site.register(Article)
--- aula/models/__init__.py
+++ aula/models/__init__.py
@@ -1,3 +1,6 @@
 from .base_model import *
 from .passport import *
 from .person import *
+from .article import *
+from .reporter import *
+
